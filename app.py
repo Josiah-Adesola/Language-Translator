24511b904b7f7efdcf3d559eb9d2c47d55aacdba
Josiah Adesola <59745353+Josiah-Adesola@users.noreply.github.com>
2023-03-08 10:17:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -6,14 +6,14 @@ openai.api_key = st.secrets["api_key"]
 
 def main():
     st.title("Polygot - The Language Translator")
-    st.write("NaijaLingo translates english language to 4 major languages, French, Spanish, Arabic and Chinese")
+    st.write("NaijaLingo translates english language to 5 major languages, French, Spanish, Arabic, Chinese and Greek")
     st.image("translate.png")
     words = st.text_area("Type an english sentence:")
     if st.button("Translate"):
         with st.spinner("Translating.."):
             response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=f"Translate this into 1. French, 2. Spanish 3. Arabic and 4. Chinese  with the language in front:\n\nProduct: {words}",
+            prompt=f"Translate this into 1. French, 2. Spanish 3. Arabic and 4. Chinese 5. Greek with the language in front:\n\nProduct: {words}",
             temperature=0.2,
             max_tokens=100,
             top_p=1,
